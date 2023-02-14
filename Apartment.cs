5dde43da84efab14b4d2eae1500a977271824720
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-02-14 10:13:03 -0800

    Update 2

--- Apartment.cs
+++ Apartment.cs
@@ -24,11 +24,25 @@ namespace Midterm
             _lastName = lastName;
             _monthlyPayment = monthlyPayment;
             _numberOfBedrooms = numberOfBedrooms;
+            _isOccupied = true;
         }
 
         public Apartment(string apartmentNumber)
         {
-
+            _isOccupied = false;
+            _firstName = "";
+            _lastName = "";
+            _monthlyPayment = 0;
+            _numberOfBedrooms = 0;
+            
         }
+        public string ApartmentNumber { get => _apartmentNumber; set => _apartmentNumber = value; }
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public decimal MonthlyPayment { get => _monthlyPayment; set => _monthlyPayment = value; }
+        public decimal CurrentBalance { get => _currentBalance; set => _currentBalance = value; }
+        public float NumberOfBedrooms { get => _numberOfBedrooms; set => _numberOfBedrooms = value; }
+        public string ApartmentNotes { get => _apartmentNotes; set => _apartmentNotes = value; }
+        public bool IsOccupied { get => _isOccupied; set => _isOccupied = value; }
     }
 }

397f635377cdf69297364ac7a10250997a2ba7be
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-02-14 09:37:44 -0800

    Starting
    1

---
+++ Apartment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    public class Apartment
+    {
+        string _apartmentNumber;
+        string _firstName;
+        string _lastName;
+        decimal _monthlyPayment;
+        decimal _currentBalance;
+        float _numberOfBedrooms;
+        string _apartmentNotes;
+        bool _isOccupied;
+
+        public Apartment(string apartmentNumber, string firstName, string lastName, decimal monthlyPayment, float numberOfBedrooms)
+        {
+            _apartmentNumber = apartmentNumber;
+            _firstName = firstName;
+            _lastName = lastName;
+            _monthlyPayment = monthlyPayment;
+            _numberOfBedrooms = numberOfBedrooms;
+        }
+
+        public Apartment(string apartmentNumber)
+        {
+
+        }
+    }
+}
